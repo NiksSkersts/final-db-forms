7c806292de6dd6c77b970ee403fabdaaa5e30dbc
NiksSkersts <skersts98@outlook.lv>
2021-05-11 01:46:59 +0300

    Add project files.

---
+++ Main.Designer.cs
@@ -0,0 +1,294 @@
+﻿
+namespace final_db_forms
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.llu = new final_db_forms.llu();
+            this.food_categoriesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.food_categoriesTableAdapter = new final_db_forms.lluTableAdapters.food_categoriesTableAdapter();
+            this.tableAdapterManager = new final_db_forms.lluTableAdapters.TableAdapterManager();
+            this.food_categoriesDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.recipesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.recipesTableAdapter = new final_db_forms.lluTableAdapters.recipesTableAdapter();
+            this.ingredientsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.ingredientsTableAdapter = new final_db_forms.lluTableAdapters.ingredientsTableAdapter();
+            this.ingredientsDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.llu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.food_categoriesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.food_categoriesDataGridView)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.recipesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ingredientsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ingredientsDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1392, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton3.Text = "toolStripButton3";
+            // 
+            // llu
+            // 
+            this.llu.DataSetName = "llu";
+            this.llu.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // food_categoriesBindingSource
+            // 
+            this.food_categoriesBindingSource.DataMember = "food_categories";
+            this.food_categoriesBindingSource.DataSource = this.llu;
+            // 
+            // food_categoriesTableAdapter
+            // 
+            this.food_categoriesTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.food_categoriesTableAdapter = this.food_categoriesTableAdapter;
+            this.tableAdapterManager.ingredientsTableAdapter = this.ingredientsTableAdapter;
+            this.tableAdapterManager.name_of_breadTableAdapter = null;
+            this.tableAdapterManager.nep_ingredientsTableAdapter = null;
+            this.tableAdapterManager.positionsTableAdapter = null;
+            this.tableAdapterManager.priceTableAdapter = null;
+            this.tableAdapterManager.recipesTableAdapter = this.recipesTableAdapter;
+            this.tableAdapterManager.sales_forceTableAdapter = null;
+            this.tableAdapterManager.sold_recipe_indexTableAdapter = null;
+            this.tableAdapterManager.soldTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = final_db_forms.lluTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // food_categoriesDataGridView
+            // 
+            this.food_categoriesDataGridView.AutoGenerateColumns = false;
+            this.food_categoriesDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.food_categoriesDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2});
+            this.food_categoriesDataGridView.DataSource = this.food_categoriesBindingSource;
+            this.food_categoriesDataGridView.Location = new System.Drawing.Point(8, 6);
+            this.food_categoriesDataGridView.Name = "food_categoriesDataGridView";
+            this.food_categoriesDataGridView.Size = new System.Drawing.Size(300, 714);
+            this.food_categoriesDataGridView.TabIndex = 2;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "id_category";
+            this.dataGridViewTextBoxColumn1.HeaderText = "id_category";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "name";
+            this.dataGridViewTextBoxColumn2.HeaderText = "name";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 25);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1392, 749);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.AutoScroll = true;
+            this.tabPage1.Controls.Add(this.ingredientsDataGridView);
+            this.tabPage1.Controls.Add(this.food_categoriesDataGridView);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1384, 723);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // recipesBindingSource
+            // 
+            this.recipesBindingSource.DataMember = "recipes";
+            this.recipesBindingSource.DataSource = this.llu;
+            // 
+            // recipesTableAdapter
+            // 
+            this.recipesTableAdapter.ClearBeforeFill = true;
+            // 
+            // ingredientsBindingSource
+            // 
+            this.ingredientsBindingSource.DataMember = "ingredients_fk";
+            this.ingredientsBindingSource.DataSource = this.food_categoriesBindingSource;
+            // 
+            // ingredientsTableAdapter
+            // 
+            this.ingredientsTableAdapter.ClearBeforeFill = true;
+            // 
+            // ingredientsDataGridView
+            // 
+            this.ingredientsDataGridView.AutoGenerateColumns = false;
+            this.ingredientsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ingredientsDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5});
+            this.ingredientsDataGridView.DataSource = this.ingredientsBindingSource;
+            this.ingredientsDataGridView.Location = new System.Drawing.Point(314, 6);
+            this.ingredientsDataGridView.Name = "ingredientsDataGridView";
+            this.ingredientsDataGridView.Size = new System.Drawing.Size(393, 714);
+            this.ingredientsDataGridView.TabIndex = 2;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "id_ingredients";
+            this.dataGridViewTextBoxColumn3.HeaderText = "id_ingredients";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "name";
+            this.dataGridViewTextBoxColumn4.HeaderText = "name";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.DataPropertyName = "category_id";
+            this.dataGridViewTextBoxColumn5.HeaderText = "category_id";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1392, 776);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.llu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.food_categoriesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.food_categoriesDataGridView)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.recipesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ingredientsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ingredientsDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private llu llu;
+        private System.Windows.Forms.BindingSource food_categoriesBindingSource;
+        private lluTableAdapters.food_categoriesTableAdapter food_categoriesTableAdapter;
+        private lluTableAdapters.TableAdapterManager tableAdapterManager;
+        private lluTableAdapters.recipesTableAdapter recipesTableAdapter;
+        private System.Windows.Forms.DataGridView food_categoriesDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.BindingSource recipesBindingSource;
+        private lluTableAdapters.ingredientsTableAdapter ingredientsTableAdapter;
+        private System.Windows.Forms.BindingSource ingredientsBindingSource;
+        private System.Windows.Forms.DataGridView ingredientsDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace final_db_forms
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void food_categoriesBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.food_categoriesBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.llu);
+
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'llu.ingredients' table. You can move, or remove it, as needed.
+            this.ingredientsTableAdapter.Fill(this.llu.ingredients);
+            // TODO: This line of code loads data into the 'llu.recipes' table. You can move, or remove it, as needed.
+            this.recipesTableAdapter.Fill(this.llu.recipes);
+            // TODO: This line of code loads data into the 'llu.food_categories' table. You can move, or remove it, as needed.
+            this.food_categoriesTableAdapter.Fill(this.llu.food_categories);
+
+        }
+    }
+}
